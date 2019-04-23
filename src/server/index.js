d40a94cafb89087beacc606f73abff63fbe882e8
Ryan Roemer <ryan.roemer@formidable.com>
2019-04-23 15:26:42 -0700

    Finish docs and authentication.

--- src/server/index.js
+++ src/server/index.js
@@ -5,6 +5,7 @@ const { promisify } = require("util");
 const { resolve } = require("path");
 const jsonServer = require("json-server");
 const dogs = require("dogs/dogs.json");
+const { log } = console;
 
 const existsP = promisify(fs.exists);
 const writeFileP = promisify(fs.writeFile);
@@ -20,10 +21,17 @@ const isLocaldev = STAGE === "localdev";
 const remoteFile = "db.json";
 const remoteBucket = `tf-fmd-${SERVICE_NAME}-${STAGE}-${AWS_REGION}-data`;
 
+// Read-write needs to have an API key.
+// Regenerate with `openssl rand -hex 16`
+// **NOTE**: Do **not** do real secrets like ^^^^^ 😂
+const API_KEY_HEADER = "x-dogs-api-key";
+const API_KEY_SECRET = "534d2ce3c021f7eb0a3eff708b981a40";
+
+// Check valid, matching API key secret header.
+const hasApiKey = (req) => req.headers[API_KEY_HEADER] === API_KEY_SECRET;
+
 const localDbPath = ({ isLambda }) =>
   resolve(`.db-${STAGE}-${isLambda ? "lambda" : "node"}.json`);
-
-const { log } = console;
 
 const ensureLocalDb = async ({ isLambda, reset }) => {
   const localDb = localDbPath({ isLambda });
@@ -58,36 +66,54 @@ const getAdapter = async ({ isLambda }) => {
   return await low(storage);
 };
 
+// Use sub-apps to switch between read-only and read-write versions.
+const createSubApp = async ({ isLambda, isReadOnly }) => {
+  // The base app + configuration.
+  const subApp = jsonServer.create();
+  subApp.set("json spaces", JSON_INDENT);
+  subApp.use(jsonServer.defaults({ readOnly: isReadOnly }));
+
+  // Pass through initial untouched dogs object for debugging ease
+  subApp.use("/dogs.json", (req, res) => res.json(dogs));
+
+  // Create router / internal db, configure, and enable.
+  let adapter = ORIG_DATA;
+  if (!isReadOnly) {
+    adapter = await getAdapter({ isLambda });
+  }
+  const router = jsonServer.router(adapter);
+  router.db._.id = "key";
+  subApp.use(router);
+
+  // Reset data to initial state
+  if (!isReadOnly) {
+    subApp.use("/reset", async (req, res) => {
+      // Set persistent storage (only needed on local disk).
+      if (isLocaldev) {
+        await ensureLocalDb({ isLambda, reset: true });
+      }
+
+      // Reset in-memory database.
+      await router.db.setState(ORIG_DATA);
+      res.json({ msg: "Database reset" });
+    });
+  }
+
+  return subApp;
+};
+
 let app;
 const createApp = async ({ isLambda }) => {
   // Memoize.
   if (app) { return app; }
 
-  // The base app + configuration.
-  app = jsonServer.create();
-  app.set("json spaces", JSON_INDENT);
-  app.use(jsonServer.defaults({ }));
+  // Create sub-apps.
+  const readWriteApp = await createSubApp({ isLambda, isReadOnly: false });
+  const readOnlyApp = await createSubApp({ isLambda, isReadOnly: true });
 
-  // Pass through initial untouched dogs object for debugging ease
-  app.use("/dogs.json", (req, res) => res.json(dogs));
-
-  // Create router / internal db, configure, and enable.
-  const adapter = await getAdapter({ isLambda });
-  const router = jsonServer.router(adapter);
-  router.db._.id = "key";
-  app.use(router);
-
-  // Reset data to initial state
-  app.use("/reset", async (req, res) => {
-    // Set persistent storage (only needed on local disk).
-    if (isLocaldev) {
-      await ensureLocalDb({ isLambda, reset: true });
-    }
-
-    // Reset in-memory database.
-    await router.db.setState(ORIG_DATA);
-    res.json({ msg: "Database reset" });
-  });
+  // Switch between apps based on header.
+  app = jsonServer.create(); // could be `express`, but avoid top-level dep.
+  app.use((req, res, next) => (hasApiKey(req) ? readWriteApp : readOnlyApp)(req, res, next));
 
   return app;
 };
